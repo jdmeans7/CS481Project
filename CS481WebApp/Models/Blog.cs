e2db64180737657269886ccfc383d345705a67b2
Jason <jasondmeans7@gmail.com>
2018-10-23 19:21:25 -0400

    Rescaffolded with .NET authentication

---
+++ CS481WebApp/Models/Blog.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CS481WebApp.Models
+{
+    public class Blog
+    {
+        public int ID { get; set; }
+        //Foreign key for User
+        public int AuthorID { get; set; }
+        public string Title { get; set; }
+        public string Text { get; set; }
+    }
+
+
+}
