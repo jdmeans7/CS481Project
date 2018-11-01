2a9d1d65a6f478994c5278654fc6dd747e7806a6
Jason Means <jasondmeans7@gmail.com>
2018-11-01 19:30:11 -0400

    Added baseline blog functionality.

---
+++ CS481WebApp/Controllers/BlogsController.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CS481WebApp.Models;
+using Microsoft.AspNet.Identity;
+
+namespace CS481WebApp.Controllers
+{
+    public class BlogsController : Controller
+    {
+        private Entities db = new Entities();
+
+        // GET: Blogs
+        public ActionResult Index()
+        {
+            return View(db.Blogs.ToList());
+        }
+
+        public ActionResult Manage()
+        {
+            var id = System.Web.HttpContext.Current.User.Identity.GetUserId();
+            return View(db.Blogs.Where((x => x.userID == id)).ToList());
+        }
+
+        // GET: Blogs/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Blog blog = db.Blogs.Find(id);
+            if (blog == null)
+            {
+                return HttpNotFound();
+            }
+            return View(blog);
+        }
+
+        // GET: Blogs/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Blogs/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "name,text")] Blog blog)
+        {
+            if (ModelState.IsValid)
+            {
+                var userID = System.Web.HttpContext.Current.User.Identity.GetUserId();
+                var timestamp = DateTime.Now;
+                Blog blogAdd = new Blog
+                {
+                    blogID = db.Blogs.Count(),
+                    userID = userID,
+                    name = blog.name,
+                    text = blog.text,
+                    timestamp = timestamp,
+                };
+                db.Blogs.Add(blogAdd);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(blog);
+        }
+
+        // GET: Blogs/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Blog blog = db.Blogs.Find(id);
+            if (blog == null)
+            {
+                return HttpNotFound();
+            }
+            return View(blog);
+        }
+
+        // POST: Blogs/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "blogID,userID,name,text,timestamp")] Blog blog)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(blog).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(blog);
+        }
+
+        // GET: Blogs/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Blog blog = db.Blogs.Find(id);
+            if (blog == null)
+            {
+                return HttpNotFound();
+            }
+            return View(blog);
+        }
+
+        // POST: Blogs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Blog blog = db.Blogs.Find(id);
+            db.Blogs.Remove(blog);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
