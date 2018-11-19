ef3afbd9434158fa9edcc6d21a303084df1825a2
Olivia <olivia.milam.hall@gmail.com>
2018-11-18 19:32:19 -0500

    Adding in Wenxuan branch manually due to dissimilar git histories

--- CS481WebApp/Controllers/HomeController.cs
+++ CS481WebApp/Controllers/HomeController.cs
@@ -3,12 +3,49 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using CS481WebApp.Models;
 
 namespace CS481WebApp.Controllers
 {
     public class HomeController : Controller
     {
         public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult API()
+        {
+            return View();
+        }
+
+        public ActionResult WeatherJS()
+        {
+            return View();
+        }
+
+        public ActionResult WeatherOrNot()
+        {
+            return View();
+        }
+
+        public JsonResult GetWeather()
+        {
+            Weather weath = new Weather();
+            return Json(weath.getWeatherForcast(), JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult FeedJS()
+        {
+            return View();
+        }
+
+        public ActionResult News()
+        {
+            return View();
+        }
+
+        public ActionResult Exchange()
         {
             return View();
         }
